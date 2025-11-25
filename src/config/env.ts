87c63af991167fecff7014a64965634497eea1e8
Olubukola <adebiyibukola157@gmail.com>
2025-11-25 16:55:50 +0100

    Add backend code without env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,6 @@ dotenv.config();
 export const env = {
   NODE_ENV: process.env.NODE_ENV ?? "development",
   PORT: process.env.PORT ?? "4000",
-  JWT_SECRET: process.env.JWT_SECRET ?? "dev-secret",
+  JWT_SECRET: process.env.JWT_SECRET ?? "",
   DATABASE_URL: process.env.DATABASE_URL ?? "",
 };
